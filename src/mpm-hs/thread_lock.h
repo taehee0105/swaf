35ac5f037a9613886120ebf52e481270a6640146
taehee0105 <taehee0105@naver.com>
2025-04-17 19:36:56 +0900

    뮤텍스 wrapping 헤더

---
+++ src/mpm-hs/thread_lock.h
@@ -0,0 +1,16 @@
+/* 뮤텍스 wrapping 헤더 */
+#ifndef THREAD_LOCK_H
+#define THREAD_LOCK_H
+
+#include <pthread.h>
+
+typedef pthread_mutex_t SCMutex;
+
+#define SCMUTEX_INITIALIZER       PTHREAD_MUTEX_INITIALIZER
+#define SCMutexInit(mutex, attr) pthread_mutex_init((mutex), (attr))
+#define SCMutexDestroy(mutex)    pthread_mutex_destroy((mutex))
+#define SCMutexLock(mutex)       pthread_mutex_lock((mutex))
+#define SCMutexUnlock(mutex)     pthread_mutex_unlock((mutex))
+
+#endif
+
