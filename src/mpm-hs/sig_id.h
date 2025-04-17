396f132f5edda9c85ca44dcb674a07576a3a7cf9
taehee0105 <taehee0105@naver.com>
2025-04-17 19:36:35 +0900

    패턴, 시그니처 내부 ID 타입 정의 헤더

---
+++ src/mpm-hs/sig_id.h
@@ -0,0 +1,12 @@
+/* 패턴과 시그니처 내부 ID 타입 정의 헤더 */
+
+#ifndef __WAF_TYPES_H__
+#define __WAF_TYPES_H__
+
+#include <stdint.h>
+
+typedef uint32_t SigIntId;  /* 내부 signature ID */
+typedef uint32_t PatIntId;  /* 패턴 ID (사용할 경우) */
+
+#endif // __WAF_TYPES_H__
+
