5ae90c8ca62bb0b2d9262a9726c6d5d9fca4f8bf
taehee0105 <taehee0105@naver.com>
2025-04-17 19:18:15 +0900

    16진수 패턴을 ASCII로 변환

---
+++ src/mpm-hs/hyperscan.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "mem.h"      /* SCCalloc */
+#include "hyperscan.h"
+
+char *HSRenderPattern(const uint8_t *pat, uint16_t pat_len)
+{
+    if (pat == NULL) {
+        return NULL;
+    }
+
+    const size_t hex_len = (pat_len * 4) + 1;
+    char *str = SCCalloc(1, hex_len, char);
+    if (str == NULL) {
+        return NULL;
+    }
+
+    char *sp = str;
+    for (uint16_t i = 0; i < pat_len; i++) {
+        snprintf(sp, 5, "\\x%02x", pat[i]);
+        sp += 4;
+    }
+    *sp = '\0';
+    return str;
+}
+
