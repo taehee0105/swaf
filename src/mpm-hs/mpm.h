ff7990f1fa4dad065a2912c511bca1e7c23469f2
taehee0105 <taehee0105@naver.com>
2025-04-18 18:58:12 +0900

    MPM 엔진 API 정의 헤더

--- src/mpm-hs/mpm.h
+++ src/mpm-hs/mpm.h
@@ -1,12 +1,13 @@
 /* MPM 엔진 API 정의 헤더 */
 
-#ifndef __WAF_MPM_H__
-#define __WAF_MPM_H__
+#ifndef SWAF_MPM_H
+#define SWAF_MPM_H
 
 #include <stdint.h>
-#include "mpm_ctx.h"     /* MpmCtx, MpmThreadCtx 구조체 정의 */
-#include "sig_id.h"      /* SigIntId 정의 */
-#include "prefilter.h"   /* PrefilterRuleStore 정의 */
+
+#include "mpm_ctx.h"
+#include "prefilter.h"
+#include "sig_id.h"
 
 /* Matcher 타입 정의 (필요 시 확장 가능) */
 enum {
@@ -15,9 +16,8 @@ enum {
     MPM_TABLE_SIZE,
 };
 
-/**
- * MPM 엔진을 위한 인터페이스 함수들
- */
+
+/* MPM 엔진을 위한 인터페이스 함수들 */
 
 /**
  * MPM 컨텍스트 초기화
@@ -26,47 +26,32 @@ enum {
  */
 void MpmInitCtx(MpmCtx *mpm_ctx, uint8_t matcher);
 
-/**
- * MPM 쓰레드 컨텍스트 초기화
- */
+/* MPM 쓰레드 컨텍스트 초기화 */
 void MpmInitThreadCtx(MpmThreadCtx *mpm_thread_ctx, uint16_t matcher);
 
-/**
- * MPM 쓰레드 컨텍스트 해제
- */
+/* MPM 쓰레드 컨텍스트 해제 */
 void MpmDestroyThreadCtx(MpmThreadCtx *mpm_thread_ctx, const uint16_t matcher);
 
-/**
- * 패턴 등록 (Case-Sensitive)
- */
+/* 패턴 등록 (Case-Sensitive) */
 int MpmAddPatternCS(MpmCtx *mpm_ctx, uint8_t *pat, uint16_t patlen,
                     uint16_t offset, uint16_t depth, uint32_t pid,
                     SigIntId sid, uint8_t flags);
 
-/**
- * 패턴 등록 (Case-Insensitive)
- */
+/* 패턴 등록 (Case-Insensitive) */
 int MpmAddPatternCI(MpmCtx *mpm_ctx, uint8_t *pat, uint16_t patlen,
                     uint16_t offset, uint16_t depth, uint32_t pid,
                     SigIntId sid, uint8_t flags);
 
-/**
- * 일반 패턴 등록
- */
+/* 일반 패턴 등록 */
 int MpmAddPattern(MpmCtx *mpm_ctx, uint8_t *pat, uint16_t patlen,
                   uint16_t offset, uint16_t depth, uint32_t pid,
                   SigIntId sid, uint8_t flags);
 
-/**
- * 패턴 메모리 해제
- */
+/* 패턴 메모리 해제 */
 void MpmFreePattern(MpmCtx *mpm_ctx, MpmPattern *p);
 
-/**
- * 실제 패턴 매칭 수행 (검색)
- */
+/* 실제 패턴 매칭 수행 (검색) */
 uint32_t MpmSearch(const MpmCtx *mpm_ctx, MpmThreadCtx *thread_ctx,
                    PrefilterRuleStore *pmq, const uint8_t *buf, uint32_t buflen);
 
-#endif /* __WAF_MPM_H__ */
-
+#endif /* SWAF_MPM_H */
