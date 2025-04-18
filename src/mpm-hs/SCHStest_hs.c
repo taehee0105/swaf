0d01b1c2003b889d946f9f7372f3935612faca76
taehee0105 <taehee0105@naver.com>
2025-04-18 19:00:27 +0900

    유닛 테스트 메인

--- src/mpm-hs/SCHStest_hs.c
+++ src/mpm-hs/SCHStest_hs.c
@@ -1,15 +1,18 @@
+/* 유닛 테스트 메인 */
+
 #define PCRE2_CODE_UNIT_WIDTH 8
 
 #include <pcre2.h>
 #include <stddef.h>
+
+#include "mpm_hs.h"
 #include "unittest.h"
-#include "util-mpm-hs.h"
 
 int main(void) {
     MpmHSRegister(); 
     UtInitialize();
-    SCHSRegisterTests();  // SCHSTestXX 등록 함수
-    UtRunTests(NULL);     // 전체 테스트 실행
+    SCHSRegisterTests();  /* SCHSTest 등록 함수 */
+    UtRunTests(NULL);     /* 전체 테스트 실행 */
     UtCleanup();
     return 0;
 }
