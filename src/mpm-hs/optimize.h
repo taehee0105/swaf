72a90c2bccdb1d4e6d8c05383e8b79f14de38d0e
taehee0105 <taehee0105@naver.com>
2025-04-18 18:58:48 +0900

    조건 분기 최적화(likely/unlikely), 메모리 정렬/동기화 최적화 매크로 정의 헤더

--- src/mpm-hs/optimize.h
+++ src/mpm-hs/optimize.h
@@ -1,7 +1,7 @@
-/* 조건 분기 최적화(likely/unlikely)와 메모리 정렬/동기화 최적화 매크로 헤더 */
+/* 조건 분기 최적화(likely/unlikely), 메모리 정렬/동기화 최적화 매크로 정의 헤더 */
 
-#ifndef __WAF_OPTIMIZE_H__
-#define __WAF_OPTIMIZE_H__
+#ifndef SWAF_OPTIMIZE_H
+#define SWAF_OPTIMIZE_H
 
 #if defined(__GNUC__) || defined(__clang__)
     #ifndef likely
@@ -16,9 +16,20 @@
     #define unlikely(x) (x)
 #endif
 
-/* 메모리 배리어 (필요할 경우만 사용) */
+
+/* memory barrier (필요할 경우만 사용) */
+
+/**
+ * 컴파일러 최적화 방지 
+ * 메모리 접근 순서가 컴파일러의 재배치에 의해 변경되는 걸 막음
+ */
 #define cc_barrier() __asm__ __volatile__("" ::: "memory")
+
+/**
+ * h/w memory barrier 
+ * 멀티코어 환경에서 CPU가 명령어를 out-of-order 실행하지 않도록 강제
+ * __sync_synchronize() -> GCC 내장 함수 (read/write 메모리 접근 순서 보장)
+ */
 #define hw_barrier() __sync_synchronize()
 
-#endif // __WAF_OPTIMIZE_H__
-
+#endif /* SWAF_OPTIMIZE_H */
