f7327a9cdf5d01d935892f0bb0aaba8ab9380973
taehee0105 <taehee0105@naver.com>
2025-04-18 18:57:10 +0900

    mpm_hs 헤더

---
+++ src/mpm-hs/mpm_hs.h
@@ -0,0 +1,11 @@
+#ifndef SWAF_MPM_HS_H
+#define SWAF_MPM_HS_H
+
+void MpmHSRegister(void);
+
+void MpmHSGlobalCleanup(void);
+
+/* 유닛 테스트를 위한 함수 */
+void SCHSRegisterTests(void);   
+
+#endif /* SWAF_MPM_HS_H */
